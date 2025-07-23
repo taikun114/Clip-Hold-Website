3d3aab40949461ff7e8cb554598d2a1096431796
たいくん <98800458+taikun114@users.noreply.github.com>
2025-07-23 16:11:33 +0900

    Update website

--- app/__generated__/$resources.sitemap.xml.ts
+++ app/__generated__/$resources.sitemap.xml.ts
@@ -2,83 +2,83 @@
       export const sitemap = [
   {
     "path": "/",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/privacy-policy",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/privacy-policy",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/support",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/support",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/release-notes",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/release-notes",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/docs",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/docs/faq",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/docs/standard-phrases-and-copy-history",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/docs/standard-phrases-window-and-history-window",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/docs/notifications",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/jp/docs/settings",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/docs",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/docs/faq",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/docs/standard-phrases-and-copy-history",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/docs/standard-phrases-window-and-history-window",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/docs/notifications",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   },
   {
     "path": "/docs/settings",
-    "lastModified": "2025-07-22"
+    "lastModified": "2025-07-23"
   }
 ];
     
