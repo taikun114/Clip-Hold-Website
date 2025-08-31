1daa8e6f968a6a81b23d437833d5e6bc72665569
たいくん <98800458+taikun114@users.noreply.github.com>
2025-08-31 21:17:42 +0900

    Update website

--- app/__generated__/[upgrade-history-datahttps:].[clip-hold.taikun.design].[jp].[docs].[upg]._index.ts
+++
@@ -1,4 +0,0 @@
-
-        export const url = "/docs/upgrade-history-data";
-        export const status = 301;
-        
--- app/routes/[upgrade-history-datahttps:].[clip-hold.taikun.design].[jp].[docs].[upg]._index.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from "react-router";
-import { url, status } from "../__generated__/[upgrade-history-datahttps:].[clip-hold.taikun.design].[jp].[docs].[upg]._index";
-
-export const loader = () => {
-  throw redirect(url, status);
-};
