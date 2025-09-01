c9a67d38079fc38469456df3a488d8909cd163da
たいくん <98800458+taikun114@users.noreply.github.com>
2025-09-01 18:37:41 +0900

    Update website

--- app/__generated__/$resources.sitemap.xml.ts
+++ app/__generated__/$resources.sitemap.xml.ts
@@ -2,107 +2,107 @@
       export const sitemap = [
   {
     "path": "/",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/privacy-policy",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/privacy-policy",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/support",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/support",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/release-notes",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/release-notes",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs/faq",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs/standard-phrases-and-copy-history",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs/standard-phrases-window-and-history-window",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs/notifications",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs/settings",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs/faq",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs/standard-phrases-and-copy-history",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs/standard-phrases-window-and-history-window",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs/notifications",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs/settings",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs/upgrade-history-data",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs/upgrade-history-data",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs/useful-usages",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs/useful-usages",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/jp/docs/export-and-import-of-standard-phrases",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   },
   {
     "path": "/docs/export-and-import-of-standard-phrases",
-    "lastModified": "2025-08-31"
+    "lastModified": "2025-09-01"
   }
 ];
     
--- app/__generated__/[jp].[docs].[faq]._index.tsx
+++ app/__generated__/[jp].[docs].[faq]._index.tsx
@@ -11,7 +11,7 @@ import { NavigationMenu as NavigationMenu, NavigationMenuList as NavigationMenuL
 
       export const projectId = "a910d043-5ab4-41b7-8e15-5b9eb80d0c41";
 
-      export const lastPublished = "2025-08-31T12:54:49.731Z";
+      export const lastPublished = "2025-09-01T09:37:09.267Z";
 
       export const siteName = "Clip Hold";
 
