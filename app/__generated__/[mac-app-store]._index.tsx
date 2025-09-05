e7244edc4e59ec3a7a96265b82c201fcfdef9110
たいくん <98800458+taikun114@users.noreply.github.com>
2025-09-05 20:22:49 +0900

    Update website

--- app/__generated__/[mac-app-store]._index.tsx
+++ app/__generated__/[mac-app-store]._index.tsx
@@ -10,7 +10,7 @@ import { HeadSlot as HeadSlot, HeadMeta as HeadMeta, MarkdownEmbed as MarkdownEm
 
       export const projectId = "a910d043-5ab4-41b7-8e15-5b9eb80d0c41";
 
-      export const lastPublished = "2025-09-05T06:32:28.193Z";
+      export const lastPublished = "2025-09-05T11:22:29.451Z";
 
       export const siteName = "Clip Hold";
 
