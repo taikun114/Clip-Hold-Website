d3a9d32574b0dca3d4f09ced35da91b1b5813dfd
たいくん <98800458+taikun114@users.noreply.github.com>
2025-10-05 00:09:11 +0900

    Update website

--- app/__generated__/[jp].[buy-me-a-coffee]._index.tsx
+++ app/__generated__/[jp].[buy-me-a-coffee]._index.tsx
@@ -10,7 +10,7 @@ import { HeadSlot as HeadSlot, HeadMeta as HeadMeta, MarkdownEmbed as MarkdownEm
 
       export const projectId = "a910d043-5ab4-41b7-8e15-5b9eb80d0c41";
 
-      export const lastPublished = "2025-10-04T09:59:24.914Z";
+      export const lastPublished = "2025-10-04T14:50:27.211Z";
 
       export const siteName = "Clip Hold";
 
